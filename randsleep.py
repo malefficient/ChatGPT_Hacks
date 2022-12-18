0eef7f5ea89d1e897fd7e815a9de06b037b4538c
Johnny Cache <johnycsh@gmail.com>
2022-12-17 19:23:14 -0800

    Need my ranndsleep.

---
+++ randsleep.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# chatgpt_whatlast.py:   see what everyone else is asking ChatGPT
+import random
+import time
+r = random.randint(10,120)
+print("Sleeping for %02d s" % (r))
+time.sleep(r)
